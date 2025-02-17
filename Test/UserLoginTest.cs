0dae5e0a1247fec6827febb8b51b5308757cd84d
Gonzalo Albito Méndez <gmendez@tecnosylva.com>
2025-02-17 10:38:13 +0100

    chore: added test 3

--- Test/UserLoginTest.cs
+++ Test/UserLoginTest.cs
@@ -5,14 +5,22 @@ namespace UserLoginKata.Test;
 
 public class UserLoginTest
 {
-    protected const string TEST_USERNAME = "MyAwesomeUsername";
-    protected static readonly string[] TEST_USERNAMES = { "MyAwesomeUsername0", "MyAwesomeUsername1", "MyAwesomeUsername2" };
+    protected const string TEST_LOCAL_USERNAME = "MyAwesomeUsername";
+    protected static readonly List<string> TEST_LOCAL_USERNAMES = new(){ TEST_LOCAL_USERNAME+"1", TEST_LOCAL_USERNAME+"2", TEST_LOCAL_USERNAME+"3" };
+    
+    protected const string TEST_FACEBOOK_USERNAME = "MyFacebookUsername";
+    protected const string TEST_FACEBOOK_PASSWORD = "MyFacebookPassword";
+    protected static readonly Dictionary<string, string> TEST_FACEBOOK_USERS = new() {
+                                                                                    {TEST_FACEBOOK_USERNAME+"1", TEST_FACEBOOK_PASSWORD+"1"},
+                                                                                    {TEST_FACEBOOK_USERNAME+"2", TEST_FACEBOOK_PASSWORD+"2"},
+                                                                                    {TEST_FACEBOOK_USERNAME+"3", TEST_FACEBOOK_PASSWORD+"3"},
+                                                                                };
     
     [Fact]
     public void UserShouldLogInIfNotLoggedIn()
     {
         UserLoginService service = new UserLoginService();
-        User user = new User(TEST_USERNAME);
+        User user = new User(TEST_LOCAL_USERNAME);
         string result = service.ManualLogin(user);
 
         Assert.Equal(UserLoginService.SUCCESS_LOGIN, result);
@@ -22,8 +30,8 @@ public class UserLoginTest
     public void UserShouldNotLogInIfAlreadyLoggedIn()
     {
         UserLoginService service = new UserLoginService();
-        User user = new User(TEST_USERNAME);
-        User repeatedUser = new User(TEST_USERNAME);
+        User user = new User(TEST_LOCAL_USERNAME);
+        User repeatedUser = new User(TEST_LOCAL_USERNAME);
         
         service.ManualLogin(user);
         string result = service.ManualLogin(repeatedUser);
@@ -35,13 +43,27 @@ public class UserLoginTest
     public void ShouldReturnTheLoggedInUsers()
     {
         UserLoginService service = new UserLoginService();
-        foreach(string username in TEST_USERNAMES)
+        foreach(string username in TEST_LOCAL_USERNAMES)
         {
             User user = new User(username);
             service.ManualLogin(user);
         }
 
-        Assert.Equal(TEST_USERNAMES.Length, service.LoggedInUsers.Count());
-        Assert.Collection(service.LoggedInUsers, x=>TEST_USERNAMES.Contains(x));
+        Assert.Equal(TEST_LOCAL_USERNAMES.Length, service.LoggedInUsers().Count());
+       //Assert.Collection(service.LoggedInUsers, x=>TEST_USERNAMES.Contains(x));
+    }
+    
+    [Fact]
+    public void ShouldReturnExternalSessionsForAService()
+    {
+        UserLoginService service = new UserLoginService();
+        FacebookSessionManager fbService = new FacebookSessionManager();
+        
+        foreach(KeyValuePair<string, string> user in TEST_FACEBOOK_USERS)
+        {
+            fbService.Login(user.Key, user.Value);
+        }
+        
+        Assert.Equal(TEST_FACEBOOK_USERS.Count, service.GetExternalSessions());
     }
 }
--- src/UserLoginService.cs
+++ src/UserLoginService.cs
@@ -4,10 +4,10 @@ public class UserLoginService
 {
     public const string SUCCESS_LOGIN = "User successfully logged in";
     public const string ERROR_ALREADY_LOGGED_IN = "User successfully logged in";
-    
+
     private List<User> _loggedUsers = new();
 
-    public IEnumerable<string> LoggedInUsers => this._loggedUsers.Select(x => x.UserName);
+    public IEnumerable<string> LoggedInUsers() => this._loggedUsers.Select(x => x.UserName);
 
     public string ManualLogin(User user)
     {
@@ -15,6 +15,7 @@ public class UserLoginService
         {
             return SUCCESS_LOGIN;
         }
+
         _loggedUsers.Add(user);
         return ERROR_ALREADY_LOGGED_IN;
     }
@@ -22,5 +23,11 @@ public class UserLoginService
     private bool IsUserAlreadyLogged(User user)
     {
         return _loggedUsers.Any(loggedUser => loggedUser.UserName == user.UserName);
+    }
+
+    public int GetExternalSessions()
+    {
+        FacebookSessionManager facebookSessionManager = new FacebookSessionManager();
+        return facebookSessionManager.GetSessions();
     }
 }
