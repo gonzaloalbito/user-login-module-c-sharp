e99fc1e3af7db99dc41a8286be1acd3d4ffb2c49
Gonzalo Albito Méndez <gmendez@tecnosylva.com>
2025-02-17 09:36:53 +0100

    chore: Test user repeated

--- Test/UserLoginTest.cs
+++ Test/UserLoginTest.cs
@@ -5,13 +5,29 @@ namespace UserLoginKata.Test;
 
 public class UserLoginTest
 {
+    protected const string TEST_USERNAME = "user";
+    
     [Fact]
     public void ShouldLogAUser()
     {
         var service = new UserLoginService();
 
-        var result = service.ManualLogin();
+        User user = new User(TEST_USERNAME);
+        string result = service.ManualLogin(user);
 
-        Assert.Equal("user logged", result);
+        Assert.Equal(UserLoginService.SUCCESS_LOGIN, result);
+    }
+    
+    [Fact]
+    public void ShouldNotLogTwice()
+    {
+        var service = new UserLoginService();
+
+        User user = new User(TEST_USERNAME);
+        User repeatedUser = new User(TEST_USERNAME);
+        service.ManualLogin(user);
+        var result = service.ManualLogin(repeatedUser);
+        
+        Assert.Equal(UserLoginService.ERROR_ALREADY_LOGGED_IN, result);
     }
 }
--- src/UserLoginService.cs
+++ src/UserLoginService.cs
@@ -2,11 +2,20 @@ namespace UserLoginKata.Src;
 
 public class UserLoginService
 {
+    public const string SUCCESS_LOGIN = "User successfully logged in";
+    public const string ERROR_ALREADY_LOGGED_IN = "User successfully logged in";
+    
+    
     private List<User> _loggedUsers = new();
 
-    public string ManualLogin()
+    public string ManualLogin(User user)
     {
-        return "user logged";
+        if (IsUserAlreadyLogged(user))
+        {
+            return SUCCESS_LOGIN;
+        }
+        _loggedUsers.Add(user);
+        return ERROR_ALREADY_LOGGED_IN;
     }
 
     private bool IsUserAlreadyLogged(User user)
